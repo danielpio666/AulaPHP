2d76b9174b0c622861a1f3731c92533e9dce4486
Daniel Pio de Magalhaes <danielpio666@gmail.com>
2015-04-28 13:50:15 -0300

    Correção sujerida pelo instrutor

--- _opcoes.php
+++ _opcoes.php
@@ -1,17 +1,7 @@
 <?php
-	if ($pag == "home") {
-		include "home.php";
-	} elseif ($pag == "empresa") {	
-		include "empresa.php";
-	} elseif ($pag == "produtos") {	
-		include "produtos.php";
-	} elseif ($pag == "servicos") {	
-		include "servicos.php";
-	} elseif ($pag == "contato") {	
-		include "contato.php";
-	} elseif ($pag == "enviar") {	
-		include "enviar.php";
-	} else {
-		echo "<script>window.location.href='index.php?pagina=home';</script>";
-	}
+$paginas = array('home','empresa','servicos','produtos','contato','enviar');
+if(in_array($pag, $paginas))
+    include $pag.".php";
+else 
+    echo "<script>window.location.href='index.php?pagina=home';</script>";
 ?>
